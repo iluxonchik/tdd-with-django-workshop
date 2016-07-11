6692e080d4ef99f9b4084a8b294435b0b1283901
ILUXONCHIK <iluxon4ik@hotmail.com>
2016-07-11 17:20:32 +0100

    add unit tests and make them pass

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -5,7 +5,11 @@ from lists.views import home_page
 
 class HomePaheViewTest(TestCase):
 
-    def test_home_page_returns_correct_html(self):
+    # NOTE on csrf: to make the unit test work with csrf tokens, we need to make sure that we're always using the same HttpRequest
+    # object, since for a different HttpRequest object, a different value of csrf token will be generated, so the assertEqual() tests
+    # will fail.
+
+    def test_home_page_uses_home_template(self):
         request = HttpRequest()  # represents the user's HTTP request
         response = home_page(request)  # just calling the view function directly
         
@@ -13,6 +17,18 @@ class HomePaheViewTest(TestCase):
         # and test behaviour (expected content, and not constants).
         # Note, we did not remove the tests above, we only do that after we make sure that the tests
         # continue passing.
-        expected_content = render_to_string('home.html')
+        expected_content = render_to_string('home.html', request=request)  # request=request to make sure the same csrf token is generated
         self.assertEqual(response.content.decode('utf-8'), expected_content)
+
+    def test_home_page_can_store_post_requests(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'new item'
+
+        # request=request to make sure the same csrf token is generated
+        expected_content = render_to_string('home.html', {'new_item_text': request.POST['item_text']}, request=request)
+
+        response = home_page(request)
+
+        self.assertEqual(expected_content, response.content.decode('utf8'))
 
