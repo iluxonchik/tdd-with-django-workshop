57a2b4f167ddad47ecfc296b10fbffabbfeb0b93
ILUXONCHIK <iluxon4ik@hotmail.com>
2016-07-13 21:43:50 +0100

    move urls to lists app

---
+++ superlists/lists/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from lists import views
+
+urlpatterns = [
+    url(r'^new/$', views.new_list, name='new_list'),
+    url(r'^(?P<list_id>[0-9]+)/add_item/$', views.add_item, name='add_item'),
+    url(r'^(?P<list_id>[0-9]+)/$', views.view_list, name='view_list'),
+]
